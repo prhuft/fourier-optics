08f7021a17d01badb15a4ed20ab05db2fcb020d3
prhuft <31295508+prhuft@users.noreply.github.com>
2020-05-22 15:28:14 -0500

    fft Talbot analysis working

--- field_funcs.py
+++ field_funcs.py
@@ -3,7 +3,7 @@ Quality-of-life functions for my fft and field calculations
 """
 
 ## modules. I don't import all from numpy to avoid subtle problems in importing numpy elsewhere
-from numpy import linspace, meshgrid, sqrt, arctan2, pi, argmax, flip, zeros
+from numpy import linspace, meshgrid, sqrt, arctan2, pi, argmax, flip, zeros, append
 import matplotlib.pyplot as plt
 
 def figax(roi=None, xlabel=None, ylabel=None, aspect='equal'):
@@ -90,3 +90,64 @@ def from_quadrant3(qd3, field=None):
     field[midx:, :midy] = flip(qd3, axis=0)
     
     return field
+
+
+def zero_pad(field, thiccness):
+    """
+    pad the field on each edge with zeros
+    
+    Args:
+        field: 2D array to be padded
+        thiccness: number of rows and columns of zeros to be added to field
+    Returns:
+        2D array of shape (field.shape[0]+2*thiccness, field.shape[1]+2*thiccness)
+    """
+    
+    rows, cols = field.shape
+    # pad left/right edges
+    field = append(field, zeros((rows, thiccness)), axis=1)
+    field = append(zeros((rows, thiccness)), field, axis=1)
+    # pad top/bottom edges
+    cols = field.shape[1]
+    field = append(field, zeros((thiccness, cols)), axis=0)
+    field = append(zeros((thiccness, cols)), field, axis=0)
+    
+    return field
+    
+def unpad(field, thiccness):
+    """
+    unpad the field on each edge given the padding thiccness
+    
+    Args:
+        field: 2D array to be padded
+        thiccness: number of rows and columns to be removed
+    Returns:
+        2D array of shape (field.shape[0]-2*thiccness, field.shape[1]-2*thiccness)
+    """
+    
+    field = field[thiccness:,:]
+    field = field[:-thiccness,:]
+    field = field[:,thiccness:]
+    field = field[:,:-thiccness]
+
+    return field
+    
+def circ_mask(rgrid, radius):
+    """
+    create a mask that is 1 within some radius and 0 otherwise
+    Args:
+        rgrid: the radius grid to use for evaluating the radius equality
+        radius: the radius, in the same units as rgrid
+    Return:
+        mask: the binary mask, a 2D array with same dimensions as rgrid
+    """   
+    
+    rows,cols = rgrid.shape
+    
+    mask = zeros((rows,cols))
+    for i in range(cols):
+        for j in range(rows):
+            if rgrid[i,j] < radius:
+                mask[i,j] = 1
+                
+    return mask
